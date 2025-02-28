400d01859a967bf22a905863816378244c32ab71
Weedy404 <envergure404@gmail.com>
2025-02-28 18:06:14 +0900

    [Silver IV] Title: 카드2, Time: 204 ms, Memory: 53584 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		
+		Queue<Integer> card = new LinkedList<>();
+		
+		for(int i =1; i<=N; i++) {
+			card.offer(i);
+		}
+		int temp=1;
+		
+		while(card.size()>1) {
+			card.poll();
+			temp = card.poll();
+			card.offer(temp);
+		}
+		
+		bw.write(String.valueOf(temp));
+		bw.newLine();
+		bw.flush();
+
+		br.close();
+		bw.close();
+	}
+}
